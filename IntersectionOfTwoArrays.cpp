0bff00c0b699c7498692aa1ba3b1ce17b906be16
husenbhagat <49280980+husenbhagat@users.noreply.github.com>
2019-05-21 18:38:27 -0700

    Create IntersectionOfTwoArrays.cpp

---
+++ IntersectionOfTwoArrays.cpp
@@ -0,0 +1,17 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+class Solution {
+public:
+	int singleNumber(vector<int>& nums) {
+		sort(nums.begin(), nums.end());
+		for (size_t i = 0; i < nums.size(); i+=2) {
+			if (i == nums.size() - 1) return nums[i];
+			if (nums[i] != nums[i + 1])
+			{
+				return nums[i];
+			}
+		}
+		return nums[nums.size() - 1];
+	}
+};
