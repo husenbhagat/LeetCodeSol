985d00e902e23f4a2ce9b48cfcc4d2e8dc48464f
husenbhagat <49280980+husenbhagat@users.noreply.github.com>
2019-05-22 15:16:36 -0700

    Update BestTimetoBuyandSellStockII.cpp

--- BestTimetoBuyandSellStockII.cpp
+++ BestTimetoBuyandSellStockII.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
 	int maxProfit(std::vector<int>& prices) {
-		int profit = 0;
+		int profit = 0;//
 		if (prices.size() == 0) return profit;
 		for (size_t i = 0; i < prices.size(); i++) {
 			if ((i + 1) < prices.size()) {
